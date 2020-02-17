c8e6cdeddda3bba1152678fd8c7cfff807b3b6b1
Jan Gillich <jan.gillich@tink.se>
2020-02-17 20:46:27 +0100

    feat(core implementation): Remove rpc, models, and service modules

--- service/src/main/java/com/tink/link/service/misc/DateTimeConverters.kt
+++
@@ -1,6 +0,0 @@
-package com.tink.link.service.misc
-
-import com.google.protobuf.Timestamp
-import org.threeten.bp.Instant
-
-internal fun Timestamp.toInstant() = Instant.ofEpochSecond(seconds, nanos.toLong())
