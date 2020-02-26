74b7523b06033545b8e0ca95712a2d52e62e79fb
Jan Gillich <jan.gillich@tink.se>
2020-02-26 15:33:45 +0100

    doc(TinkExtensions): Link to `Tink` object

--- link/src/main/java/com/tink/link/TinkExtensions.kt
+++ link/src/main/java/com/tink/link/TinkExtensions.kt
@@ -7,7 +7,7 @@ import com.tink.service.handler.ResultHandler
 
 
 /**
- * This is for Java compatibility only. Use extensions on `Tink` when using Kotlin
+ * This is for Java compatibility only. Use extensions on [Tink] when using Kotlin
  */
 @kotlin.SinceKotlin(version = "999999999.0.0")
 object TinkLink {
