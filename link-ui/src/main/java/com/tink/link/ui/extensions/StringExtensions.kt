55715e573624fbe6db1fe1082c1f279ebf0bfcd8
Mohan Kumar <mohan.kumar@tink.se>
2020-03-30 16:02:17 +0200

    feat(call2action): Added extension to do CTA styling for text

---
+++ link-ui/src/main/java/com/tink/link/ui/extensions/StringExtensions.kt
@@ -0,0 +1,47 @@
+package com.tink.link.ui.extensions
+
+import android.content.Context
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.SpannedString
+import android.text.TextPaint
+import android.text.style.ClickableSpan
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
+import com.tink.link.ui.R
+
+internal fun String.convertCallToActionText(
+    ctaText: String,
+    action: () -> Unit,
+    context: Context
+): CharSequence {
+    val startIndex = indexOf(ctaText)
+    val spannableString = SpannableString.valueOf(this)
+        .apply {
+            setSpan(
+                TinkCallToActionSpan(context, action),
+                startIndex,
+                startIndex + ctaText.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+    return SpannedString(spannableString)
+}
+
+internal class TinkCallToActionSpan(
+    val context: Context,
+    val action: () -> Unit
+) : ClickableSpan() {
+
+    override fun updateDrawState(ds: TextPaint) {
+        ds.apply {
+            isUnderlineText = false
+            color = context.getColorFromAttr(R.attr.tink_colorPrimary)
+            typeface = ResourcesCompat.getFont(context, R.font.tink_font_semi_bold)
+        }
+    }
+
+    override fun onClick(widget: View) {
+        action.invoke()
+    }
+}
