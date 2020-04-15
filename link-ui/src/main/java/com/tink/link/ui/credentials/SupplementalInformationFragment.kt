7b8a9f789486af5fa198a334763080e2de530dec
Mohan Kumar <mohan.kumar@tink.se>
2020-04-15 16:29:25 +0200

    refactor(credential): Added fragment and viewmodel for supplemental info

---
+++ link-ui/src/main/java/com/tink/link/ui/credentials/SupplementalInformationFragment.kt
@@ -0,0 +1,89 @@
+package com.tink.link.ui.credentials
+
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.core.os.bundleOf
+import androidx.core.view.children
+import androidx.core.view.updatePadding
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.tink.link.ui.R
+import com.tink.link.ui.extensions.dpToPixels
+import com.tink.model.misc.Field
+import kotlinx.android.synthetic.main.tink_dialog_supplemental_information.*
+
+private const val CREDENTIALS_ID_ARG = "CREDENTIALS_ID_ARG"
+
+private const val SUPPLEMENTAL_FIELDS_ARG = "SUPPLEMENTAL_FIELDS_ARG"
+
+class SupplementalInformationFragment : DialogFragment() {
+
+    private val credentialsId: String by lazy {
+        requireNotNull(arguments?.getString(CREDENTIALS_ID_ARG))
+    }
+
+    private val supplementalFields: ArrayList<Field> by lazy {
+        requireNotNull(arguments?.getParcelableArrayList<Field>(SUPPLEMENTAL_FIELDS_ARG))
+    }
+
+    private val supplementalInformationViewModel: SupplementalInformationViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.Tink_MaterialAlertDialogStyle)
+        supplementalInformationViewModel.setData(credentialsId, supplementalFields)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.apply {
+
+            setContentView(R.layout.tink_dialog_supplemental_information)
+
+            supplementalInformationViewModel.apply {
+                supplementalFields.value?.let { fields ->
+                    for (field in fields) {
+                        supplementalFieldsContainer
+                            .addView(
+                                CredentialField(requireContext())
+                                    .also {
+                                        it.updatePadding(bottom = resources.dpToPixels(32))
+                                        it.setupField(field)
+                                    })
+                    }
+                }
+
+                submitButton.setOnClickListener {
+                    val filledFields = supplementalFieldsContainer.children
+                        .filterIsInstance(CredentialField::class.java)
+                        .map { it.getFilledField() }
+                        .toList()
+                    supplementalInformationViewModel.sendSupplementalInformation(
+                        filledFields,
+                        { dismiss() },
+                        {}
+                    )
+                }
+
+                cancelButton.setOnClickListener {
+                    supplementalInformationViewModel.cancelSupplementalInformation()
+                    dismiss()
+                }
+            }
+
+            setCanceledOnTouchOutside(false)
+        }
+
+        return dialog
+    }
+
+    companion object {
+        fun getBundle(credentialsId: String, supplementalFields: List<Field>) =
+            bundleOf(
+                CREDENTIALS_ID_ARG to credentialsId,
+                SUPPLEMENTAL_FIELDS_ARG to supplementalFields
+            )
+    }
+}
---
+++ link-ui/src/main/java/com/tink/link/ui/credentials/SupplementalInformationViewModel.kt
@@ -0,0 +1,61 @@
+package com.tink.link.ui.credentials
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.tink.core.Tink
+import com.tink.link.core.credentials.CredentialRepository
+import com.tink.link.getUserContext
+import com.tink.model.misc.Field
+import com.tink.service.handler.ResultHandler
+import com.tink.service.misc.toFieldMap
+
+class SupplementalInformationViewModel : ViewModel() {
+
+    private val credentialsId = MutableLiveData<String>()
+
+    private val _supplementalFields = MutableLiveData<List<Field>>()
+    val supplementalFields: LiveData<List<Field>> = _supplementalFields
+
+    private val credentialsRepository: CredentialRepository
+
+    init {
+        val userContext = requireNotNull(Tink.getUserContext())
+        credentialsRepository = userContext.credentialRepository
+    }
+
+    fun setData(credentialsId: String, supplementalFields: List<Field>) {
+        this.credentialsId.value = credentialsId
+        _supplementalFields.value = supplementalFields
+    }
+
+    fun sendSupplementalInformation(
+        fields: List<Field>,
+        onSuccess: () -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
+        credentialsId.value?.let { id ->
+            credentialsRepository.supplementInformation(
+                id,
+                fields.toFieldMap(),
+                ResultHandler(
+                    {
+                        onSuccess()
+                    },
+                    {
+                        onError(it)
+                    }
+                )
+            )
+        }
+    }
+
+    fun cancelSupplementalInformation() {
+        credentialsId.value?.let {
+            credentialsRepository.cancelSupplementalInformation(
+                it,
+                ResultHandler({}, {})
+            )
+        }
+    }
+}
