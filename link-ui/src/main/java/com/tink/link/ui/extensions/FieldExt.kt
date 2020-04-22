1fdd8651681ad491a9563ae8bbb0e9520a8800a0
Mohan Kumar <mohan.kumar@tink.se>
2020-04-22 15:07:00 +0200

    refactor(field): Suggested refactoring on field extension function

--- link-ui/src/main/java/com/tink/link/ui/extensions/FieldExt.kt
+++ link-ui/src/main/java/com/tink/link/ui/extensions/FieldExt.kt
@@ -2,23 +2,15 @@ package com.tink.link.ui.extensions
 
 import android.content.Context
 import android.view.View
-import com.tink.link.ui.credentials.CredentialsField
 import com.tink.link.ui.credentials.ImmutableCredentialsField
 import com.tink.link.ui.credentials.MutableCredentialsField
 import com.tink.model.misc.Field
 
-internal fun Field.toView(context: Context): View {
-    val credentialsField: CredentialsField =
-        if (isEditable()) {
-            MutableCredentialsField(context)
-        } else {
-            ImmutableCredentialsField(context)
-        }
-    return credentialsField
-        .let {
-            it.setupField(this@toView)
-            it.getView()
-        }
-}
+internal fun Field.toView(context: Context): View =
+    if (isEditable()) {
+        MutableCredentialsField(context).also { it.setupField(this) }
+    } else {
+        ImmutableCredentialsField(context).also { it.setupField(this) }
+    }
 
 internal fun Field.isEditable(): Boolean = !attributes.inputType.isImmutable || value.isEmpty()
