a706d53ff77ff42f5b5bfd85826c871138ac2106
Jan Gillich <jan.gillich@tink.se>
2020-04-29 14:52:52 +0200

    ref(core 0.3.0): Update for user refactoring

--- app/src/main/java/com/tink/link/MainFragment.kt
+++ app/src/main/java/com/tink/link/MainFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.tink.core.Tink
-import com.tink.service.authentication.user.User
+import com.tink.model.user.User
 import com.tink.service.handler.ResultHandler
 
 class MainFragment : Fragment(), TinkLinkConsumer {
