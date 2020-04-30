b35e147c533fa14d8f5e044fbe9c8c02e1e0828d
Mohan Kumar <mohan.kumar@tink.se>
2020-04-30 13:27:19 +0200

    feat(testing): Added assertj library for testing

--- link-ui/build.gradle.kts
+++ link-ui/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(Dependencies.Androidx.core_ktx)
     implementation(Dependencies.Androidx.constraint_layout)
     testImplementation(Dependencies.junit)
+    androidTestImplementation(Dependencies.Testing.test_assertj)
     androidTestImplementation(Dependencies.Androidx.test_runner)
     androidTestImplementation(Dependencies.Androidx.test_espresso)
 
