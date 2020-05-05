361cc99cbab0751dca571fe9bb1633037f82c1b5
Jan Gillich <jan.gillich@tink.se>
2020-05-05 22:07:43 +0200

    feat(sample app): Remove unused repository provider

--- app/src/main/java/com/tink/link/MainActivity.kt
+++ app/src/main/java/com/tink/link/MainActivity.kt
@@ -4,10 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.tink.core.Tink
-import com.tink.core.provider.ProviderRepository
-import com.tink.link.core.credentials.CredentialsRepository
 import com.tink.link.configuration.Configuration
 import com.tink.service.network.TinkConfiguration
 
@@ -47,16 +44,3 @@ class MainActivity : AppCompatActivity() {
 }
 
 interface TinkLinkConsumer
-
-interface TinkRepositoryProvider {
-    val providerRepository: ProviderRepository?
-    val credentialsRepository: CredentialsRepository?
-}
-
-fun <T> T.getRepositoryProvider() where T : Fragment, T : TinkLinkConsumer =
-    object : TinkRepositoryProvider {
-        override val providerRepository: ProviderRepository?
-            get() = Tink.getUserContext()?.providerRepository
-        override val credentialsRepository: CredentialsRepository?
-            get() = Tink.getUserContext()?.credentialsRepository
-    }
