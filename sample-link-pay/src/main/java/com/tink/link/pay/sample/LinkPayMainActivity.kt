650d439484223a2c9c349ff8e716f4d26c9d2da9
Jan Gillich <jan.gillich@tink.se>
2020-05-11 18:22:03 +0200

    feat(pay): Setup sample configuration

--- sample-link-pay/src/main/java/com/tink/link/pay/sample/LinkPayMainActivity.kt
+++ sample-link-pay/src/main/java/com/tink/link/pay/sample/LinkPayMainActivity.kt
@@ -1,13 +1,35 @@
 package com.tink.link.pay.sample
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.tink.core.Tink
+import com.tink.link.createTemporaryUser
+import com.tink.link.pay.sample.configuration.Configuration
+import com.tink.service.handler.ResultHandler
+import com.tink.service.network.TinkConfiguration
 
 class LinkPayMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Tink.init(
+            TinkConfiguration(
+                Configuration.sampleEnvironment,
+                Configuration.sampleOAuthClientId,
+                Uri.parse("tinklink://sample/pay")
+            ),
+            applicationContext
+        )
+
+        Tink.createTemporaryUser("SE", "en_US", ResultHandler({
+            Log.d(localClassName, "User created");
+            Tink.setUser(it)
+        }, {
+            Log.e(localClassName, "Error:", it);
+        }))
     }
 }
