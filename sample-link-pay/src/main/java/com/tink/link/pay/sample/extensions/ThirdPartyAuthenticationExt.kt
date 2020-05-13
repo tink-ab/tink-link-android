8da5315dd576b8335b8dc212aca2bba705f36dfa
Jan Gillich <jan.gillich@tink.se>
2020-05-13 21:58:42 +0200

    feat(pis): copy third party launcher from AIS

---
+++ sample-link-pay/src/main/java/com/tink/link/pay/sample/extensions/ThirdPartyAuthenticationExt.kt
@@ -0,0 +1,32 @@
+package com.tink.link.pay.sample.extensions
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.tink.link.R
+import com.tink.model.authentication.ThirdPartyAppAuthentication
+
+fun ThirdPartyAppAuthentication.launch(activity: Activity, onCancel: (() -> Unit)? = null) {
+    val thirdPartyAuthenticationAndroid = requireNotNull(android)
+
+    val thirdPartyIntent = Intent().apply {
+        action = Intent.ACTION_VIEW
+        data = Uri.parse(thirdPartyAuthenticationAndroid.intent)
+        addCategory("android.intent.category.BROWSABLE")
+        addCategory("android.intent.category.DEFAULT")
+    }
+
+    try {
+        activity.startActivityForResult(thirdPartyIntent, 0)
+    } catch (exception: ActivityNotFoundException) {
+        val launchByPackageIntent = activity.packageManager
+            .getLaunchIntentForPackage(thirdPartyAuthenticationAndroid.packageName)
+        if (launchByPackageIntent != null) {
+            activity.startActivityForResult(launchByPackageIntent, 0)
+        } else {
+            TODO()
+        }
+    }
+}
