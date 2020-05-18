60003c373a9f4d39d12e193aa1bf1ae4527cf284
Mohan Kumar <mohan.kumar@tink.se>
2020-05-18 14:06:54 +0200

    refactor(access-type): Design changes to access type screen
    Use the same list UX as in other provider tree node screens.

--- link-ui/src/main/java/com/tink/link/ui/accesstype/AccessTypeListFragment.kt
+++ link-ui/src/main/java/com/tink/link/ui/accesstype/AccessTypeListFragment.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
-import com.squareup.picasso.Picasso
-import com.tink.link.ui.credentialstype.CredentialsTypeListFragment
 import com.tink.link.ui.R
 import com.tink.link.ui.extensions.toArrayList
+import com.tink.link.ui.providernode.ProviderNodeFragment
 import com.tink.link.ui.providertree.ARG_PROVIDER_TOOLBAR_TITLE
 import com.tink.link.ui.providertree.ARG_PROVIDER_TREE
 import com.tink.link.ui.providertree.ProviderTreeNodeFragment
-import com.tink.model.provider.Provider
 import com.tink.model.provider.ProviderTreeNode
-import kotlinx.android.synthetic.main.tink_fragment_access_type_list.*
 import kotlinx.android.synthetic.main.tink_layout_toolbar.*
 
 /**
@@ -28,61 +25,21 @@ class AccessTypeListFragment :
         requireNotNull(arguments?.getString(ARG_PROVIDER_TOOLBAR_TITLE))
     }
 
-    private val providerList: List<ProviderTreeNode> by lazy {
-        requireNotNull(arguments?.getParcelableArrayList<ProviderTreeNode>(ARG_PROVIDER_TREE))
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (providerList.size == 1) {
-            // Skip this level and go directly to the child,
-            // instead of displaying a list of only one possible selection.
-            findNavController().popBackStack()
-            navigateToNode(providerList.first())
-        }
-    }
-
-    override fun setupViews() {
-        toolbar.title = getString(R.string.tink_access_type_fragment_title, toolbarTitle)
-        setupCardViews()
-    }
-
-    private fun setupCardViews() {
-        providerList
-            .filterIsInstance<ProviderTreeNode.AccessTypeNode>()
-            .let { accessTypeList ->
-                accessTypeList
-                    .firstOrNull { it.type == Provider.AccessType.OPEN_BANKING }
-                    ?.let { openBankingItem ->
-                        openBankingItem.icon?.let {
-                            Picasso.get().load(it).into(openBankingLogo)
-                            openBankingLogo.visibility = View.VISIBLE
-                        }
-                        openBankingAddButton.setOnClickListener { navigateToNode(openBankingItem) }
-                        openBankingAccessTypeCard.visibility = View.VISIBLE
-                    }
-                accessTypeList
-                    .firstOrNull { it.type == Provider.AccessType.OTHER }
-                    ?.let { otherItem ->
-                        otherItem.icon?.let {
-                            Picasso.get().load(it).into(otherLogo)
-                            otherLogo.visibility = View.VISIBLE
-                        }
-                        otherAddButton.setOnClickListener { navigateToNode(otherItem) }
-                        otherAccessTypeCard.visibility = View.VISIBLE
-                    }
-            }
+        toolbar.title = getString(R.string.tink_access_type_fragment_title)
     }
 
     /**
-     * Navigate to the [CredentialsTypeListFragment]
+     * Navigate to the [ProviderNodeFragment]
      */
     override fun navigateToNode(node: ProviderTreeNode) {
         findNavController().navigate(
             R.id.credentialsTypeListFragment,
             bundleOf(
-                ARG_PROVIDER_TREE to (node as ProviderTreeNode.AccessTypeNode).credentialsTypes.toArrayList(),
-                ARG_PROVIDER_TOOLBAR_TITLE to toolbarTitle
+                ARG_PROVIDER_TOOLBAR_TITLE to toolbarTitle,
+                ARG_PROVIDER_TREE to (node as ProviderTreeNode.AccessTypeNode).credentialsTypes.toArrayList()
             )
         )
     }
