8d0958c28a227335c369974103324735c815bb9f
Jan Gillich <jangillich@users.noreply.github.com>
2020-05-28 16:31:20 +0200

    doc: Commit suggestion
    Co-authored-by: Magnus Gudmandsen <maggud@users.noreply.github.com>

--- link-payments/src/main/java/com/tink/link/payments/TransferRepository.kt
+++ link-payments/src/main/java/com/tink/link/payments/TransferRepository.kt
@@ -156,7 +156,7 @@ internal class TransferRepositoryImpl(
 }
 
 /**
- * The status of a transfer
+ * The status of a transfer.
  * Possible values are [Success], [Loading], and [AwaitingAuthentication]
  */
 sealed class TransferStatus {
