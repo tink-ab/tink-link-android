ba0fc6a39b50bd5aad29ac1a90e5513429e19ec8
Jan Gillich <jan.gillich@tink.se>
2020-05-28 17:59:47 +0200

    ref(transfer): s/CredentialsError/AuthenticationError

--- link-payments/src/main/java/com/tink/link/payments/TransferFailure.kt
+++ link-payments/src/main/java/com/tink/link/payments/TransferFailure.kt
@@ -3,7 +3,7 @@ package com.tink.link.payments
 class TransferFailure(val reason: Reason) : Throwable() {
 
     sealed class Reason(val message: String?) {
-        class CredentialsError(message: String? = null) : Reason(message)
+        class AuthenticationError(message: String? = null) : Reason(message)
         class TransferFailed(message: String? = null) : Reason(message)
     }
 }
