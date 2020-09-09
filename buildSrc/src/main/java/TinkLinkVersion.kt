96c079340e7b200e453f777fc16ad9beffcb04d1
Mohan Kumar <mohan.kumar@tink.se>
2020-09-09 13:41:30 +0200

    chore(version): Bump version to 0.13.2

--- buildSrc/src/main/java/TinkLinkVersion.kt
+++ buildSrc/src/main/java/TinkLinkVersion.kt
@@ -2,7 +2,7 @@ object TinkLinkVersion {
 
     private const val major = 0
     private const val minor = 13
-    private const val patch = 1
+    private const val patch = 2
 
     const val name = "$major.$minor.$patch"
 
