69bfdcbf9ceff67d9320434cfa0059f00ea994cc
Mohan Kumar <mohan.kumar@tink.se>
2020-10-14 15:47:23 +0200

    chore(formatting): Run ktLintFormat

--- link-ui/src/main/java/com/tink/link/ui/providerlist/ProviderListFragment.kt
+++ link-ui/src/main/java/com/tink/link/ui/providerlist/ProviderListFragment.kt
@@ -7,7 +7,6 @@ import androidx.core.graphics.drawable.DrawableCompat
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
