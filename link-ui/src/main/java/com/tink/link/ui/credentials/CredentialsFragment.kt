24b25e141b314547c65e66700a492f4a4aaebf05
Mohan Kumar <mohan.kumar@tink.se>
2020-10-26 16:30:27 +0100

    chore(formatting): Run ktLintFormat

--- link-ui/src/main/java/com/tink/link/ui/credentials/CredentialsFragment.kt
+++ link-ui/src/main/java/com/tink/link/ui/credentials/CredentialsFragment.kt
@@ -14,7 +14,6 @@ import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
