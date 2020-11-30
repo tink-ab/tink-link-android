1068fcacef6b7f14a81666778c4ef0ce98d1812c
Mohan Kumar <mohan.kumar@tink.se>
2020-11-30 14:12:35 +0100

    feat(link-ui): Add TinkLinkTracker

---
+++ link-ui/src/main/java/com/tink/link/ui/analytics/TinkLinkTracker.kt
@@ -0,0 +1,33 @@
+package com.tink.link.ui.analytics
+
+import com.tink.link.ui.CredentialsOperation
+import com.tink.link.ui.analytics.models.AppInfo
+import com.tink.link.ui.analytics.models.InteractionEvent
+import com.tink.link.ui.analytics.models.ScreenEvent
+import com.tink.link.ui.analytics.models.toFlowInfo
+import com.tink.link.ui.analytics.network.AnalyticsService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+
+internal object TinkLinkTracker {
+
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    fun initialize(clientId: String, userId: String, appInfo: AppInfo, operation: CredentialsOperation) {
+        AnalyticsService.initialize(clientId, userId, appInfo, operation.toFlowInfo())
+    }
+
+    fun trackScreen(screenEvent: ScreenEvent) {
+        scope.launch {
+            AnalyticsService.sendScreenEvent(screenEvent)
+        }
+    }
+
+    fun trackInteraction(interactionEvent: InteractionEvent, screenEvent: ScreenEvent) {
+        scope.launch {
+            AnalyticsService.sendInteractionEvent(interactionEvent, screenEvent)
+        }
+    }
+}
