3cd779c75c9efd578a401f7cea78c21b7726c346
Mohan Kumar <mohan.kumar@tink.se>
2020-12-01 14:52:46 +0100

    feat(link-ui): Add Unit response for api calls

--- link-ui/src/main/java/com/tink/link/ui/analytics/network/AnalyticsApi.kt
+++ link-ui/src/main/java/com/tink/link/ui/analytics/network/AnalyticsApi.kt
@@ -5,6 +5,7 @@ import com.squareup.moshi.JsonClass
 import com.tink.link.ui.analytics.models.AnalyticsEventTypeDto
 import com.tink.link.ui.analytics.models.InteractionEventDto
 import com.tink.link.ui.analytics.models.ViewEventDto
+import retrofit2.Response
 import retrofit2.http.POST
 
 internal interface AnalyticsApi {
@@ -12,12 +13,12 @@ internal interface AnalyticsApi {
     @POST("/link/v1/analytics")
     suspend fun sendViewEvent(
         @retrofit2.http.Body body: ViewEventRequest
-    )
+    ): Response<Unit>
 
     @POST("/link/v1/analytics")
     suspend fun sendInteractionEvent(
         @retrofit2.http.Body body: InteractionEventRequest
-    )
+    ): Response<Unit>
 }
 
 @JsonClass(generateAdapter = true)
