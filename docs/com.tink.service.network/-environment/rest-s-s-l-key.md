6090ae39652a1c015237f5a5454b5c143309f4a3
Mohan Kumar Gopalakrishnan <mohan.kumar@tink.com>
2020-12-16 16:46:46 +0100

    Release 0.14.5

--- docs/com.tink.service.network/-environment/rest-s-s-l-key.md
+++
@@ -1,5 +0,0 @@
-[link](../../index.md) / [com.tink.service.network](../index.md) / [Environment](index.md) / [restSSLKey](./rest-s-s-l-key.md)
-
-# restSSLKey
-
-`val restSSLKey: `[`String`](https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/-string/index.html)`?`
--- link-ui/src/main/java/com/tink/link/ui/analytics/network/AnalyticsService.kt
+++ link-ui/src/main/java/com/tink/link/ui/analytics/network/AnalyticsService.kt
@@ -8,22 +8,11 @@ import com.tink.link.ui.analytics.models.InteractionEventDto
 import com.tink.link.ui.analytics.models.ProductDto
 import com.tink.link.ui.analytics.models.ScreenEvent
 import com.tink.link.ui.analytics.models.ViewEventDto
-import com.tink.service.network.Environment
 import okhttp3.OkHttpClient
 import org.threeten.bp.ZonedDateTime
 import org.threeten.bp.format.DateTimeFormatter
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import java.io.ByteArrayInputStream
-import java.io.InputStream
-import java.security.KeyStore
-import java.security.cert.CertificateFactory
-import java.security.cert.X509Certificate
-import javax.net.ssl.SSLContext
-import javax.net.ssl.SSLSocketFactory
-import javax.net.ssl.TrustManager
-import javax.net.ssl.TrustManagerFactory
-import javax.net.ssl.X509TrustManager
 
 internal object AnalyticsService {
 
@@ -32,17 +21,7 @@ internal object AnalyticsService {
 
     private val product: ProductDto = ProductDto.CREDENTIALS
 
-    private val client =
-        OkHttpClient.Builder()
-            .apply {
-                Environment.Production.restSSLKey?.let { sslKey ->
-                    sslSocketFactory(
-                        TLSHelper.getSslSocketFactory(ByteArrayInputStream(sslKey.toByteArray())),
-                        TLSHelper.getFirstTrustManager(ByteArrayInputStream(sslKey.toByteArray()))
-                    )
-                }
-            }
-            .build()
+    private val client = OkHttpClient()
 
     private val retrofit =
         Retrofit.Builder()
@@ -132,42 +111,5 @@ internal object AnalyticsService {
                 )
             )
         )
-    }
-}
-
-internal object TLSHelper {
-
-    @Throws(Exception::class)
-    private fun getTrustManagers(ca: InputStream): Array<TrustManager> {
-        val ks = KeyStore.getInstance(KeyStore.getDefaultType())
-        ks.load(null)
-        val cf = CertificateFactory.getInstance("X.509")
-        val cert = cf.generateCertificate(ca) as X509Certificate
-        val principal = cert.subjectX500Principal
-        ks.setCertificateEntry(principal.getName("RFC2253"), cert)
-        // Set up trust manager factory to use our key store.
-        val trustManagerFactory =
-            TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
-        trustManagerFactory.init(ks)
-        return trustManagerFactory.trustManagers
-    }
-
-    @Throws(Exception::class)
-    fun getSslSocketFactory(ca: InputStream?): SSLSocketFactory {
-        if (ca == null) {
-            return SSLSocketFactory.getDefault() as SSLSocketFactory
-        }
-
-        val context = SSLContext.getInstance("TLS")
-        context.init(null, getTrustManagers(ca), null)
-        return context.socketFactory
-    }
-
-    fun getFirstTrustManager(ca: InputStream): X509TrustManager {
-        val trustManagers = getTrustManagers(ca)
-        check(trustManagers.size == 1 && trustManagers[0] is X509TrustManager) {
-            ("Unexpected default trust managers: ${trustManagers.contentToString()}")
-        }
-        return trustManagers[0] as X509TrustManager
     }
 }
